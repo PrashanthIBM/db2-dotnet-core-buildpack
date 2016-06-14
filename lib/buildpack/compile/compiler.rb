25e7093c66f53cda5dd36e1a3f908631ef1a9db8
PRASHANTHRAOK <PrashanthIBM@users.noreply.github.com>
2016-06-14 21:28:43 +0530

    Update compiler.rb

--- lib/buildpack/compile/compiler.rb
+++ lib/buildpack/compile/compiler.rb
@@ -70,8 +70,8 @@ module AspNetCoreBuildpack
     end
     
     def extract_clidriver(out)
-      clidriver_installer.extract(build_dir, out) unless File.exist? File.join(build_dir, 'odbc_cli') 
-      #clidriver_installer.extract(build_dir, out)
+      #clidriver_installer.extract(build_dir, out) unless File.exist? File.join(build_dir, 'odbc_cli') 
+      clidriver_installer.extract(build_dir, out)
     end
 
     def restore_dependencies(out)
