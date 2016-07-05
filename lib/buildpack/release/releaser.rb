78a3c6a800474dc0ba8f0ef507e9bb378699c10b
PRASHANTHRAOK <PrashanthIBM@users.noreply.github.com>
2016-07-05 15:31:52 +0530

    Update releaser.rb

--- lib/buildpack/release/releaser.rb
+++ lib/buildpack/release/releaser.rb
@@ -42,12 +42,12 @@ module AspNetCoreBuildpack
       File.open(startup_script, 'w') do |f|
         f.write 'export HOME=/app;'
         if ibmdb.eql?('true')
-          puts("clidriver lib path is not set \n")
+          #puts("clidriver lib path is not set \n")
           f.write 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/libunwind/lib;'
           #cmd = "echo 'LD_LIBRARY_PATH = ';echo $LD_LIBRARY_PATH;"
           #@shell.exec(cmd, @out)
         else
-          puts("clidriver path is set \n")
+          #puts("clidriver path is set \n")
           f.write 'export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/libunwind/lib:$HOME/odbc_cli/clidriver/lib;'
           #puts("LD_LIBRARY_PATH = $LD_LIBRARY_PATH")
           #cmd = "echo 'LD_LIBRARY_PATH = ';echo $LD_LIBRARY_PATH;"
