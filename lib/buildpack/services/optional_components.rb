0606b64732c9c4d1dd966f5a30f87d26574d1336
PRASHANTHRAOK <PrashanthIBM@users.noreply.github.com>
2016-07-05 13:05:27 +0530

    Update optional_components.rb

--- lib/buildpack/services/optional_components.rb
+++ lib/buildpack/services/optional_components.rb
@@ -20,28 +20,28 @@ module AspNetCoreBuildpack
   $cliinstall=false   
    
    class OptionalComponents
-     def initialize(build_dir,shell,out, vcap_services)
-        parse_vcap_services(vcap_services)
-        install_optional_components(build_dir, shell, out)
+     def initialize(build_dir, shell, out, optlCpts)
+        #parse_vcap_services(vcap_services)
+        install_optional_components(build_dir, shell, out,optlCpts)
      end
      
-     def install_optional_components(build_dir, shell, out)
-        if $cliinstall
+     def install_optional_components(build_dir, shell, out, optlCpts)
+        if optlCpts.dashDB.eql?('true')
           puts("calling cliinstall \n") 
           Clidriver.new(build_dir, shell, out)
         end
      end
 
-     def parse_vcap_services(vcap_services)
-       unless vcap_services.nil?
-         vcap_services.each do |service_type, service_data|
-           puts("inside vcap_services parsing, service_type =  #{service_type}")   
-           if 'dashDB'.eql?(service_type)
-             $cliinstall = true 
-             puts("service_type is dashDB and set to cliinstall = #{$cliinstall} \n ")
-           end
-         end
-       end
-      end
+    # def parse_vcap_services(vcap_services)
+     #  unless vcap_services.nil?
+      #   vcap_services.each do |service_type, service_data|
+       #    puts("inside vcap_services parsing, service_type =  #{service_type}")   
+        #   if 'dashDB'.eql?(service_type)
+         #    $cliinstall = true 
+          #   puts("service_type is dashDB and set to cliinstall = #{$cliinstall} \n ")
+           #end
+         #end
+       #end
+      #end
    end
 end 
